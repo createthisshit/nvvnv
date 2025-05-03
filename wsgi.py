7ac4af2f5169b26e090ddf797c19a12b7fa9b158
createthisshit <asdsasaddsa400@gmail.com>
2025-05-04 01:54:53 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,18 @@
 import threading
 from flask import Flask
-from app import start_bot
+from app import on_startup
+from aiogram.utils import executor
 
-# Минимальное Flask-приложение для Gunicorn
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Bot is running"
 
-# Запускаем бот в отдельном потоке
+# Запускаем polling в отдельном потоке
+def start_bot():
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
 threading.Thread(target=start_bot, daemon=True).start()
 
 if __name__ == "__main__":
